5b48cef5de8275458d83a0cae2d0c81855f4dc2d
‘S.M.Mehrabul <mehrab.24csedu.001@gmail.com>
2020-05-02 05:55:36 +0600

    installOnWindows.bat Debug

--- installOnWindows.bat
+++ installOnWindows.bat
@@ -6,5 +6,6 @@ set /p receiverEmail=Enter Receiver Email:
 set /p newsHour=Enter News Hour: 
 set /p newsMinute=Enter News Minute: 
 (Echo=%monkeyEmail% %monkeyEmailPassword% %receiverEmail% %newsHour% %newsMinute%)>"config.txt"
+PAUSE
 call node installOnWindows.js
 PAUSE
